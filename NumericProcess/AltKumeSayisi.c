4fc735e911ea482d271810065ccba7227a4e0920
smlozaydin <samil.bilal.ozaydin@gmail.com>
2021-10-11 22:12:23 +0300

    Alt küme mi bulma

---
+++ NumericProcess/AltKumeSayisi.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int asalMi(int sayi);
+
+int altKumeleriAsalMi(int sayi);
+
+int main()
+{
+
+    int say = 7331;
+    printf("%d", altKumeleriAsalMi(say));
+
+    return 0;
+}
+int asalMi(int sayi)
+{
+    if (sayi == 1)
+    {
+        return 1;
+    }
+    int i;
+    for (i = 2; i <= sayi / 2; i++)
+    {
+        if (sayi % i == 0)
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+int altKumeleriAsalMi(int sayi)
+{
+    if (sayi < 100)
+    {
+        return 0;
+    }
+    while (sayi > 0)
+    {
+        if (asalMi(sayi))
+        {
+            sayi /= 10;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
