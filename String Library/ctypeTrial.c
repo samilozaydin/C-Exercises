8e12ef34ab94b536e35ac37f794d024f22682ff8
smlozaydin <samil.bilal.ozaydin@gmail.com>
2022-01-03 10:24:48 +0300

    some string.h trial

--- String Library/ctypeTrial.c
+++ String Library/ctypeTrial.c
@@ -1,11 +1,31 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 
 int main()
 {
+    /*  char hex[] = "0xa25e";
+    int i;
+    for (i = 0; *(hex + i) != '\0'; i++)
+    {
+        if (isxdigit(*(hex + i)) != 0 || *(hex + i) == 'x')
+        {
+            printf("%c is a hex char\n", *(hex + i));
+        }
+        else
+        {
+            printf("It is not a hex char\n");
+        }
+    }
+*/
 
-    char kelime[20] = "5653.0 fafsaf";
+    char snt1[40] = "sentence first";
+    char snt2[] = "different sentence";
+    strcat(snt1, snt2);
+    strncat(snt1, snt2, 3);
+    puts(snt1);
+    /* char kelime[20] = "5653.0 fafsaf";
     char *point;
     int sayi = atoi(kelime);
     printf("sayi karsiligi: %d\n", sayi);
@@ -17,6 +37,7 @@ int main()
     float sayi3;
     sscanf(kelime, "%f %s", &sayi3, olustur);
     printf("olustur : %f %s", sayi3, olustur);
+*/
 
     return 0;
 }
